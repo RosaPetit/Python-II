00265fa990d27cd6f0d3551e75148fd8042aed2c
Rosa Petit <83729618+RosaPetit@users.noreply.github.com>
2021-07-21 14:36:22 -0400

    Add files via upload

---
+++ Exercise 02.py
@@ -0,0 +1,44 @@
+'''
+Este es un comentario general que se utiliza 
+para programas y funciones
+
+'''
+
+'''Programa 1''' 
+
+#Esto es un comentario  
+#variable1= input() #La variable input es para interaccion del usuario
+
+#variable2= input() 
+
+#print(variable1+variable2)
+
+'''Programa 2 '''
+
+# si queremos trabajar con numeros tenemos que decirle a que la funcion input que los agrega
+
+#variable1= float( input("\n Esbriba un numero \n"))
+
+#variable2=  float( input("\n Escriba un numero \n")) 
+
+#print("La suma es:", variable1+variable2)
+
+
+'''Programa 3'''
+
+# Utilicemos el if
+
+if (a<100)&(a>0):
+	print("\n a es menos que 100 y mayor que cero\n ")
+
+elif a>100:
+	print("\n a es mayor que 100\n ")
+
+elif a<-100:
+	print("\n a es menor que -100\n")
+
+else:
+	print("a es menor que cero, pero mayor que -100")
+
+
+
