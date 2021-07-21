f0ad97dc8aed13ce04f0ebd5190d981ce1f18256
Rosa Petit <83729618+RosaPetit@users.noreply.github.com>
2021-07-21 14:37:05 -0400

    Add files via upload

---
+++ Exercise 04.py
@@ -0,0 +1,12 @@
+'''Promagra (ciclos o bucles)'''
+
+#Funcionamiento del ciclo while
+
+cont=0
+
+while cont<50:
+	print("\n El contador vale: \n", cont)
+	#cont= cont+1 
+	cont+=1
+
+print("Ahora hacemos otra cosa")
