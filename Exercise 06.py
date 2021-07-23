30d7364114c1e615de6b6ab6ec18c009c2917f55
Rosa Petit <83729618+RosaPetit@users.noreply.github.com>
2021-07-23 13:07:04 -0400

    Add files via upload

---
+++ Exercise 06.py
@@ -0,0 +1,17 @@
+'''Programa 5 (uso avanzado del for)'''
+
+a=range(10,100,2)
+# a=rango(de diez end diez, que llegue hasta 100, de dos en dos )
+
+suma=0
+#"suma" es para definir desde donde empezaremos a sumar  
+
+for i in a:
+	suma=suma+i
+#Luego a "suma" le sumas i que es del ciclo for que contiene el rango
+	print(i,suma)
+
+print(suma)
+
+	
+
