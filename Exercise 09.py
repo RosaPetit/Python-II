92f7d482ee56c544c8a44217ea7b24b160457908
Rosa Petit <83729618+RosaPetit@users.noreply.github.com>
2021-07-30 20:23:02 -0400

    Add files via upload

---
+++ Exercise 09.py
@@ -0,0 +1,21 @@
+'''Crea una chiquita ventana con un mensaje'''
+
+from tkinter import *
+
+#Creo la ventana prinsipal
+
+raiz=Tk()
+
+#crero la etiqueta con el texto
+
+etiq=label(raiz, text="Mama! mi hermanito me saco la lengua")
+
+#ajuste exactamente el tamano de la etiqueta
+
+etiq.0
+
+#buble principal para crear la ventana y esperar eventos
+
+
+
+raiz.mainloop()
